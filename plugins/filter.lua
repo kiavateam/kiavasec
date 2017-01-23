a4a17cbe3d59e045425bacf7b25ea84aa40c59e1
kiavateam <ava@kiava.ir>
2017-01-23 05:10:14 +0330

    Update filter.lua

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -1,7 +1,7 @@
 local function addword(msg, name)
     local hash = 'chat:'..msg.chat_id_..':badword'
     redis:hset(hash, name, 'newword')
-    return "ss \n" ..name
+    return "`code` \n" ..name
 end
 
 local function get_badword_hash(msg)
